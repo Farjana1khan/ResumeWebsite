98ec079a8c140b140a1f0b81485b31ab790a3eda
Farjana Khan <farjana09khan@gmail.com>
2022-11-13 15:41:12 +0530

    update

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   render() {
     //for un-inspect
-    //window.addEventListener("contextmenu", (e) => e.preventDefault());
+    window.addEventListener("contextmenu", (e) => e.preventDefault());
 
     return (
       <div className="demo-big-content">
